587a0d249b65fb204170a8eb3d940140889f2a3b
Micakce <micakce@gmail.com>
2020-02-14 00:12:34 -0300

    primeros pasos

---
+++ Account.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Account {
+    private int credit;
+    private ArrayList<Transaction> transactions;
+
+}
---
+++ Transaction.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class Transaction{
+    private int amount;
+    private Date date;
+    private String merchant;
+}
